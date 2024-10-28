c31f1843fcc04cabc78ad47170e789c2e642585b
ICPCMansoura <icpcmans.technical@gmail.com>
2024-10-28 16:48:54 +0300

    Update Y - Math.cpp

--- Sheet #3 Number Theory 1/Y - Math.cpp
+++ Sheet #3 Number Theory 1/Y - Math.cpp
@@ -1,43 +1,58 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-	cin.sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-	int n;
-	cin >> n;
-	map<int, int> factors;
-	int maximum = 0;
-	for (int i = 2; i * i <= n; i++)
-	{
-		while (n % i == 0)
-		{
-			factors[i]++;
-			n /= i;
-			maximum = max(maximum, factors[i]);
-		}
-	}
-	int maxpow2 = 1,counter = 0;
-	while (maxpow2 < maximum)
-	{
-		maxpow2 *= 2;
-		counter++;
-	}
-	int ans = 1, flag = 0;
-	for (auto i : factors)
-	{
-		if (i.second != maxpow2)
-		{
-			flag = 1;
-		}
-		ans *= i.first;
-	}
-	cout << ans << " " << counter + flag;
 
+#define int long long
+
+int maxFact = 0;
+map <int,int> fact;
+int PrimeFactorization(int n) {
+    vector <int> PrimeFact;
+    for (int i = 2; i * i <= n; ++i) {
+        while (n % i == 0) {
+            fact[i]++;
+            PrimeFact.push_back(i);
+            n /= i;
+            maxFact = max(maxFact, fact[i]);
+        }
+    }
+    if (n > 1) {
+        fact[n]++;
+        PrimeFact.push_back(n);
+    }
+    return PrimeFact.size();
 }
 
+void solve()
+{
+    int n;
+    cin >> n;
+    PrimeFactorization(n);
+
+    int po = 1 , TotalOp = 0 , ans = 1;
+    while(po < maxFact) {
+        po *= 2 , TotalOp++;
+    }
+    bool f = 0;
+    for (auto &[x , rep] : fact) {
+        if(rep != po) {
+            f = 1;
+        }
+        ans *= x;
+    }
+    cout << ans << ' ' << TotalOp + f << '\n';
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int testCases = 1;
+  //  cin >> testCases;
+    for (int testCase = 1 ; testCase <= testCases ; ++testCase){
+        solve();
+    }
+}
